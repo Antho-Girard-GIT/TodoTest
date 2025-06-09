2723cc3427c4a24cacf9e6d34bfb9cfd20ae8c09
Anthonny Girard <anthonnygirard@gmail.com>
2025-06-09 18:20:30 -0400

    modification visuel

--- src/App.jsx
+++ src/App.jsx
@@ -55,18 +55,18 @@ export default function App() {
 
   return (
     <div className="h-screen bg-[#0d57a1] pt-25 p-10 md:p-25">
-      <div className="flex items-center justify-center bg-zinc-200 p-10 top-25 rounded-4xl shadow-2xl/50 shadow-[#FFA14A]">
+      <div className="flex items-center justify-center bg-zinc-200 p-10 top-25 rounded-4xl shadow-2xl/50 shadow-[#000]">
         <div className="flex flex-col items-center gap-8">
           <form onSubmit={handleSubmit} className="flex justify-center gap-5">
             <input
               name="todo"
               value={newTodo}
               onChange={(e) => setNewTodo(e.target.value)}
-              className="w-50 lg:w-84 xl:w-100 rounded-md ring-4 ring-[#FFA14A] bg-blue-300 font-bold p-2 text-amber-900"
+              className="w-50 lg:w-84 xl:w-100 rounded-md ring-4 ring-[#000] bg-blue-300 font-bold p-2 text-amber-900"
             />
             <button
               type="submit"
-              className="rounded-md ring-4 ring-[#FFA14A] p-3 bg-[#0F375E] hover:bg-blue-200 hover:text-black text-stone-400 uppercase font-bold transition duration-700 hover:rotate-360"
+              className="rounded-md ring-4 ring-[#000] p-3 bg-[#0F375E] hover:bg-blue-200 hover:text-black text-[#fff] uppercase font-bold transition duration-700 hover:rotate-360"
             >
               Ajouter
             </button>  
@@ -77,7 +77,7 @@ export default function App() {
                 key={todo.id}
                 className="bg-zinc-200 px-4 py-2 rounded-xl shadow-xl border flex items-center w-full"
               >
-                <span className="flex-1 text-2xl">{todo.todoitem}</span>
+                <span className="flex-1 text-lg">{todo.todoitem}</span>
                 <button
                   onClick={() => deleteTodo(todo.id)}
                   className="bg-red-600 px-2 py-2 rounded-lg text-white hover:bg-red-700 hover:scale-110"
--- src/assets/navbar.jsx
+++ src/assets/navbar.jsx
@@ -2,9 +2,8 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div className='flex justify-center bg-[#0F375E] w-full h-15'>
-        <img src="../image/logo.png" alt="logo" className='size-15 rounded-4xl absolute left-0'/>
-        <h1 className="flex items-center text-[#999] text-3xl font-semibold text-shadow-lg/30 text-shadow-[#FFA14A]">Welcome my friend</h1>
+    <div className='flex justify-center bg-[#0F375E] w-full py-4'>
+        <h1 className="flex items-center text-[#fff] text-3xl font-bold rounded-xl p-2 ring-4 ring-[#000]">TodoList</h1>
     </div>
   )
 }
